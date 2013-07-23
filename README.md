6669b77980f97743f4b35fa2b302da110e1cfc77
Wyatt Preul <wpreul@gmail.com>
2013-07-23 00:19:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://secure.travis-ci.org/wpreul/danger-shield.png)](http://travis-ci.org/wpreul/danger-shield)
+
---
+++ test/index.js
@@ -0,0 +1,23 @@
+// Load modules
+
+var Lab = require('lab');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+describe('Danger Shield', function () {
+
+
+});
